00b7f5af95131cd80db0b1591ac2ee06fae9a934
aleksandra <kirillovaukrtechinfo@gmail.com>
2024-06-20 20:32:11 +0300

    feat: fix Permissions for android

--- src/helpers/fileHelper.ts
+++ src/helpers/fileHelper.ts
@@ -3,36 +3,32 @@ import ImageResizer from 'react-native-image-resizer';
 import {Alert, PermissionsAndroid, Platform} from 'react-native';
 
 const requestStoragePermission = async () => {
-  try {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-      {
-        title: 'Storage Permission',
-        message: 'This app needs access to your storage to load photos.',
-        buttonNeutral: 'Ask Me Later',
-        buttonNegative: 'Cancel',
-        buttonPositive: 'OK',
-      },
-    );
-    return granted === PermissionsAndroid.RESULTS.GRANTED;
-  } catch (err) {
-    console.warn(err);
-    return false;
+  if (Platform.OS === 'android') {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        {
+          title: 'Storage Permission Needed',
+          message:
+            'This app needs the storage permission to access your photos',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the storage');
+      } else {
+        console.log('Storage permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
   }
 };
 
 export const handleChooseFile = async (setFile: (file: any) => void) => {
-  const hasPermission =
-    Platform.OS === 'ios' ? true : await requestStoragePermission();
-
-  if (!hasPermission) {
-    Alert.alert(
-      'Permission Denied',
-      'You need to grant storage permission to select files.',
-    );
-    return;
-  }
-
+  await requestStoragePermission();
   await launchImageLibrary({mediaType: 'mixed'}, async response => {
     if (response.didCancel) {
       console.log('User cancelled image picker');
@@ -42,6 +38,7 @@ export const handleChooseFile = async (setFile: (file: any) => void) => {
       const asset = response.assets[0];
       if (asset.type.startsWith('image')) {
         if (asset.fileSize > 320 * 240) {
+          console.log('QQQasset', asset);
           const resizedImage = await ImageResizer.createResizedImage(
             asset.uri,
             320,
